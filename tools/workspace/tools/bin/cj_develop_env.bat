2b9e0af8d60d2e7117eb82879739791a4248d90e
chenjiang_win <chenjiangeek@163.com>
2019-09-23 14:12:18 +0800

    windows path fixed

--- tools/workspace/tools/bin/cj_develop_env.bat
+++ tools/workspace/tools/bin/cj_develop_env.bat
@@ -7,9 +7,10 @@ set WORKSPACE_PATH=%SCRIPT_PATH%..\..
 set HOME=%WORKSPACE_PATH%\home
 set XDG_CONFIG_HOME=%HOME%
 set PYTHONHOME=%WORKSPACE_PATH%\tools\python3
+set PATH=%WORKSPACE_PATH%\tools\clang\LLVM\bin;%WORKSPACE_PATH%\tools\cmake\bin;%WORKSPACE_PATH%\tools\rt_thread_env\tools\bin;%PATH%
 set PATH=%WORKSPACE_PATH%\tools\bin;%PATH%
 set PATH=%WORKSPACE_PATH%\tools\git\mingw64\bin;%WORKSPACE_PATH%\tools\git\usr\bin;%PATH%
-set PATH=%WORKSPACE_PATH%\tools\python3\Scripts;%WORKSPACE_PATH%\tools\python3;%PATH%
+set PATH=%WORKSPACE_PATH%\tools\python3;%PATH%
 set PATH=%WORKSPACE_PATH%\tools\ctags;%WORKSPACE_PATH%\tools\gnu_global\bin;%WORKSPACE_PATH%\tools\vim;%WORKSPACE_PATH%\tools\neovim\bin;%PATH%
 
 start %WORKSPACE_PATH%\tools\conemu\ConEmu64.exe -title cj_develop_env -dir "%WORKSPACE_PATH%\projects"
--- tools/workspace/tools/bin/rt_thread_env.bat
+++ tools/workspace/tools/bin/rt_thread_env.bat
@@ -1,7 +1,4 @@
 @echo off
 
-set SCRIPT_PATH=%~dp0
-set SCRIPT_DISK=%SCRIPT_PATH:~0,2%
-
-start %SCRIPT_DISK%\tools\rt_thread_env\tools\ConEmu\ConEmu64.exe
+start %WORKSPACE_PATH%\tools\rt_thread_env\tools\ConEmu\ConEmu64.exe
 
