0e4b7066991f0cd8da3e808f15d3321fed718100
chenjiang_win <chenjiangeek@163.com>
2019-10-07 20:29:26 +0800

    还是 msys2 NB, 有换回来了，嘻嘻

--- tools/workspace/tools/bin/cj_develop_env.bat
+++ tools/workspace/tools/bin/cj_develop_env.bat
@@ -1,5 +1,9 @@
 @echo off
 
+set MSYS2_PATH=d:\msys2
+
+if NOT EXIST %MSYS2_PATH%\usr\bin goto install_msys2
+
 set SCRIPT_PATH=%~dp0
 set SCRIPT_DISK=%SCRIPT_PATH:~0,2%
 set WORKSPACE_PATH=%SCRIPT_PATH%..\..
@@ -7,11 +11,22 @@ set WORKSPACE_PATH=%SCRIPT_PATH%..\..
 set HOME=%WORKSPACE_PATH%\home
 set XDG_CONFIG_HOME=%HOME%
 set PYTHONHOME=%WORKSPACE_PATH%\tools\python3
-set PATH=%WORKSPACE_PATH%\tools\clang\LLVM\bin;%WORKSPACE_PATH%\tools\cmake\bin;%WORKSPACE_PATH%\tools\rt_thread_env\tools\bin;%PATH%
-set PATH=%WORKSPACE_PATH%\tools\bin;%PATH%
-set PATH=%WORKSPACE_PATH%\tools\git\mingw64\bin;%WORKSPACE_PATH%\tools\git\usr\bin;%PATH%
-set PATH=%WORKSPACE_PATH%\tools\python3;%PATH%
-set PATH=%WORKSPACE_PATH%\tools\ctags;%WORKSPACE_PATH%\tools\gnu_global\bin;%WORKSPACE_PATH%\tools\vim;%WORKSPACE_PATH%\tools\neovim\bin;%PATH%
 
-start %WORKSPACE_PATH%\tools\conemu\ConEmu64.exe -title cj_develop_env -dir "%WORKSPACE_PATH%\projects"
+set PATH=%WORKSPACE_PATH%\tools\rt_thread_env\tools\bin;%PATH%
+set PATH=%WORKSPACE_PATH%\tools\bin;%PATH%
+set PATH=%MSYS2_PATH%\usr\bin;%MSYS2_PATH%\mingw64\bin;%MSYS2_PATH%\mingw32\bin;%PATH%
+set PATH=%PYTHONHOME%;%PATH%
+set PATH=%WORKSPACE_PATH%\tools\vim;%WORKSPACE_PATH%\tools\neovim\bin;%PATH%
+
+start %WORKSPACE_PATH%\tools\conemu\ConEmu64.exe -title chenjiang_develop_env -dir "%WORKSPACE_PATH%\projects"
+goto this_script_end
+
+:install_msys2
+set msys2="www.msys2.org"
+echo Please visit %msys2% and install msys2 as %MSYS2_PATH% 
+echo OR modify "MSYS2_PATH ENV" of this script.
+pause
+exit -1
+
+:this_script_end
 
